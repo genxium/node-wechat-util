41b94848ba6854f1cf7e49c868726c624ff25718
Wing <genxium@hotmail.com>
2017-03-16 17:32:20 +0800

    Drafted utilities for payment.

--- examples/singleton.js
+++ examples/singleton.js
@@ -3,6 +3,21 @@ const singletonEnforcer = Symbol();
 
 const baseAbsPath = __dirname + '/';
 
+const gen32bytes = function() {
+	const s4 = function() {
+		return Math.floor((1 + Math.random()) * 0x10000)
+			.toString(16)
+			.substring(1);
+	};
+	return s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
+};
+
+const getRandomInt = function(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const BackendApiCore = require(baseAbsPath + '../lib/BackendApiCore').default;
 
 class BackendApiSingleton extends BackendApiCore {
@@ -23,5 +38,50 @@ class BackendApiSingleton extends BackendApiCore {
 const instance = BackendApiSingleton.instance; 
 instance.loadConfigFileSync(baseAbsPath + './configs/fserver.conf');
 
+/* Info dict loading. */
 const webLoginInfoDict = instance.queryWebLoginInfoDictSync();
+console.log('The login info dictionary');
 console.dir(webLoginInfoDict);
+console.log('\n');
+
+const miniServerPort = 9999;
+const miniServerAsyncNotiPath = '/async-cb/v1/wechat-pubsrv/payment/notify';
+
+const parseString = require('xml2js').parseString;
+
+/* Mini server for async notification */
+const express = require('express');
+const app = express();
+
+// Body parser middleware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(miniServerAsyncNotiPath, function(req, res) {
+	console.dir(req.body);
+});
+
+app.listen(miniServerPort, function () {
+  console.log('Mini server listening on port ' + miniServerPort)
+
+	/* Unified order API of `NATIVE` type. */
+	const outTradeNo = gen32bytes(); 
+	const notifyUrl = 'http://localhost:' + miniServerPort + miniServerAsyncNotiPath;
+	const nonceStr = gen32bytes();
+	const body = 'This is a testing order';
+	const totalFeeCents = getRandomInt(100, 10000);
+	const tradeType = 'NATIVE';
+	const openId = null;
+	const limitPay = null;
+
+	instance.queryUnifiedOrderRespAsync(outTradeNo, notifyUrl, nonceStr, body, totalFeeCents, tradeType)
+	.then(function(respBody) {
+		parseString(respBody, function (err, result) {
+			console.log('Response from payment server is');
+			console.dir(result);
+		});
+	});
+});
